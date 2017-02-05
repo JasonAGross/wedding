5f3e8b8d8403299a0870688496469d6bc2c78ae3
Jason Gross <jason@jasonagross.com>
2017-02-05 11:04:35 -0500

    Logic, content, styling
    Completed logical loop, added some styling

--- src/groomsmen.js
+++ src/groomsmen.js
@@ -9,26 +9,55 @@ var config = {
 var firebaseApp = firebase.initializeApp(config);
 var db = firebaseApp.database()
 
-new Vue({
-  el: '#groomsmen-container',
-  firebase: {
-    groomsmen: db.ref('groomsmen')
-  },
-  methods: {
-  	confirmSpot: function (event) {
-	  console.log(getParameterByName('user'))
+var app = new Vue({
+	el: '#app',
+	firebase: {
+		groomsman: {
+			source: db.ref('groomsmen/' + getParameterByName('user')),
+			asObject: true
+		},
+		groomsmen: db.ref('groomsmen')
+	},
+	methods: {
+		confirmSpot: function (event) {
+			db.ref('groomsmen/' + getParameterByName('user')).update({
+				status: true,
+				viewed: true
+			})
+		},
+		declineSpot: function (event) {
+			db.ref('groomsmen/' + getParameterByName('user')).update({
+				status: false,
+				viewed: true
+			})
+		}
   	}
-  }
 })
 
 function getParameterByName(name, url) {
-    if (!url) {
-      url = window.location.href;
-    }
-    name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
+	if (!url) {
+	  url = window.location.href;
+	}
+	name = name.replace(/[\[\]]/g, "\\$&");
+	var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+		results = regex.exec(url);
+	if (!results) return null;
+	if (!results[2]) return '';
+	return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+function getUser(user) {
+	var fullName = null;
+	switch (user) {
+		case 'ryan':
+			fullName = 'Ryan Strouse';
+			break;
+		case 'dan':
+			fullName = 'Dan Gaines';
+			break;
+		case 'erik':
+			fullName = 'Erik Gross';
+			break;
+	}
+	return fullName;
 }
