3748eb29205480b22bbdeb8df430ba302a5e327f
Jason Gross <jason@jasonagross.com>
2017-03-04 10:04:24 -0500

    New page + style updates

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ var app = new Vue({
   data: {
     links: [
       { title: 'Our Story', address: "index.html", active: true },
-      { title: 'Our Wedding', address: "wdding.html", active: false },
+      { title: 'Our Wedding', address: "wedding.html", active: false },
       { title: 'RSVP', address: "rsvp.html", active: false },
       { title: 'Guest Info', address: "info.html", active: false }
     ]
